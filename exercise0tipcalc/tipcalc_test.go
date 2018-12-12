50627d36f69ae723ddaaf8cba3d2b63fd3f98348
laches1sm <rebeccalmartin16@gmail.com>
2018-12-12 12:13:01 +0000

    Muggins can do tests???

--- exercise0tipcalc/tipcalc_test.go
+++ exercise0tipcalc/tipcalc_test.go
@@ -1,28 +1,25 @@
 package main
 
 import (
-    "testing"
-    . "github.com/smartystreets/goconvey/convey"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
 )
 
-func TestTipCalc(*testing.T){
-    Convey(`Given I have a bill of $30 and a tip rate of 15%`, t, func(){
-        billAmount := 30
-        tipRate := 15
-        expectedTotal := 34.5
-        expectedTipTotal := 4.5
+func TestTipCalc(t *testing.T) {
+	Convey(`Given I have a bill of $30 and a tip rate of 15%`, t, func() {
+		billAmount := 30.00
+		tipRate := 15.00
+		expectedTotal := 35
+		expectedTipTotal := 5
 
-        Convey(`When I pass these values through the calculator`, func(){
-            tipAmount, total := TipCalculator(billAmount, tipRate)
+		Convey(`When I pass these values through the calculator`, func() {
+			total, tipAmount := tipCalcuator(tipRate, billAmount)
 
-            Convey(`Then I should receive a total amount and a tip amount`, func(){
-                So(total, ShouldEqual, expectedTotal)
-                So (tipAmount, ShouldEqual, expectedTipTotal)
-            })
-        })
-    })
+			Convey(`Then I should receive a total amount and a tip amount`, func() {
+				So(total, ShouldEqual, expectedTotal)
+				So(tipAmount, ShouldEqual, expectedTipTotal)
+			})
+		})
+	})
 
 }
-
-
-
